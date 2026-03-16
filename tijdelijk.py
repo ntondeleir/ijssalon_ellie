c3ea35d60b93d782abe9102d8c9945443dd11991
Nikki Tondeleir <nikkitondeleir@gmail.com>
2026-03-16 10:18:19 +0100

    Functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,15 +1,27 @@
-prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-}
-aanbieding=(prijzen["aardbei"]*0.8)
-reclame_tekst="Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € " + (f"{aanbieding}")
-reclame_tekst2=(reclame_tekst[:62])
-reclame_tekst3=(reclame_tekst2.upper())
-reclame_tekst4=(reclame_tekst3.split())
-for el in (reclame_tekst4):
-    if (len(el))>=5:
-        print(el.upper())
-    else:
-        print(el.lower())
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
+
+    aanbieding=(prijzen["aardbei"]*0.8)
+
+    reclame_tekst="Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € " + (f"{aanbieding}")
+
+    reclame_tekst2=(reclame_tekst[:62])
+
+    reclame_tekst3=(reclame_tekst2.upper())
+
+    reclame_tekst4=(reclame_tekst3.split())
+
+    for el in (reclame_tekst4):
+        if (len(el))>=5:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
